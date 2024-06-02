154edee23e7f75758836cd47e0b23db92e4f7fa5
v-ostrovskaya <105979793+v-ostrovskaya@users.noreply.github.com>
2024-06-02 20:47:27 +0300

    glm.h ver 2

--- glm.h
+++ glm.h
@@ -2,9 +2,23 @@
 #define GLM_H
 
 #include <iostream>
+
 class vec3 {
 public:
     float a, b, c;
-    vec3(float a, float b, float c) : a(a), b(b), c(c) {}
+    vec3(float _a, float _b, float _c) : a(_a), b(_b), c(_c) {}
 };
+
+class mat4 {
+public:
+    float matrix[4][4];
+    mat4() {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                matrix[i][j] = 0.0f;
+            }
+        }
+    }
+};
+
 #endif 
